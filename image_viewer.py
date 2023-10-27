df4d417d15aa1a304a610771b1774037424f1763
NNikoGG <geekgaming222@gmail.com>
2023-10-28 04:25:02 +0530

    Added a status bar

--- image_viewer.py
+++ image_viewer.py
@@ -4,19 +4,24 @@ from PIL import ImageTk,Image
 root = Tk()
 root.title('Image Viewer')
 
-# Loading all the images
+
+# Loading the images
 my_img1 = ImageTk.PhotoImage(Image.open("images/goofy1.png"))
 my_img2 = ImageTk.PhotoImage(Image.open("images/goofy2.png"))
 my_img3 = ImageTk.PhotoImage(Image.open("images/goofy3.png"))
 my_img4 = ImageTk.PhotoImage(Image.open("images/goofy4.png"))
 my_img5 = ImageTk.PhotoImage(Image.open("images/goofy5.png"))
 
-# Storing them in a list
+# Storing the images in a list
 image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
+
+# Displaying a status bar
+status = Label(root, text="Image 1 of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
 
 # Displaying the first image
 my_label = Label(image=my_img1)
 my_label.grid(row=0, column=0, columnspan=3)
+
 
 # Defining the functions of the buttons
 def forward(image_number):
@@ -38,6 +43,12 @@ def forward(image_number):
 	button_back.grid(row=1, column=0)
 	button_forward.grid(row=1, column=2)
 
+    # Updated status bar
+	status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+	status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+	
+		
+
 def back(image_number):
 	global my_label
 	global button_forward
@@ -48,7 +59,7 @@ def back(image_number):
 	button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
 	button_back = Button(root, text="<<", command=lambda: back(image_number-1))
 
-    # Disabling the back button on first image
+    # Disabling previous button on first image
 	if image_number == 1:
 		button_back = Button(root, text="<<", state=DISABLED)
 
@@ -56,7 +67,10 @@ def back(image_number):
 	button_back.grid(row=1, column=0)
 	button_forward.grid(row=1, column=2)
 
-
+	# Updated Status Bar
+	status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+	status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+	
 button_back = Button(root, text="<<", command=back, state=DISABLED)
 button_exit = Button(root, text="Exit Program", command=root.quit)
 button_forward = Button(root, text=">>", command=lambda: forward(2))
@@ -64,6 +78,8 @@ button_forward = Button(root, text=">>", command=lambda: forward(2))
 
 button_back.grid(row=1, column=0)
 button_exit.grid(row=1, column=1)
-button_forward.grid(row=1, column=2)
+button_forward.grid(row=1, column=2, pady=10)
+status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+
 
 root.mainloop()
