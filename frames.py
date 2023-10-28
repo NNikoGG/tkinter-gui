0dd7372ad7f59889884236b1559d391160e35f7e
NNikoGG <geekgaming222@gmail.com>
2023-10-29 01:01:53 +0530

    Added frames.py

---
+++ frames.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+root = Tk()
+
+myFrame = LabelFrame(root, text= "Test Frame", padx=10, pady=10)
+myFrame.pack(padx=50, pady=50)
+
+myLabel = Label(myFrame, text= "This is a frame!")
+myLabel.pack()
+
+myButton = Button(myFrame, text= "Exit Program", command=root.quit)
+myButton.pack()
+
+
+root.mainloop()
