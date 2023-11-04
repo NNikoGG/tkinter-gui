e0ed19c0cc6c6768d4c15e42c601af095a939dbf
NNikoGG <geekgaming222@gmail.com>
2023-11-04 21:18:44 +0530

    Added dropdown.py

--- dropdown.py
+++ dropdown.py
@@ -1,5 +1,4 @@
 from tkinter import *
-from PIL import ImageTk, Image
 import emoji
 
 root = Tk()
@@ -7,8 +6,18 @@ root.title("Dropdown Menus")
 root.geometry("400x400")
 
 def show():
-    my_label = Label(root, text=clicked.get())
+    selected = clicked.get()
+    emoji_dict = {
+        "Arsenal": "Bottle Team :face_with_tears_of_joy:",
+        "Chelsea": "Finished Team :face_with_tears_of_joy:",
+        "Manchester United": "Finished Team :face_with_tears_of_joy:",
+        "Manchester City": "Oil Club :face_with_tears_of_joy:",
+        "Tottenham Hotspurs": "No Trophy :face_with_tears_of_joy:"
+    }
+    emoji_text = emoji_dict.get(selected, "")
+    my_label = Label(root, text=emoji.emojize(emoji_text))
     my_label.pack()
+
 options = [
     "Arsenal",
     "Chelsea",
@@ -17,16 +26,12 @@ options = [
     "Tottenham Hotspurs"
 ]
 
-
 clicked = StringVar()
 clicked.set(options[0])
-
 drop = OptionMenu(root, clicked, *options)
 drop.pack()
 
-
 b = Button(root, text="Show selection", command=show)
 b.pack()
-
 
 root.mainloop()
